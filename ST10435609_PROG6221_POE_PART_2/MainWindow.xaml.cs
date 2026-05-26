964f3d045b7b2b2a65424e2d132bef7cbd2eccf9
Sifiso Khausela <st10435609@rcconnect.edu.za>
2026-05-26 21:10:45 +0200

    added logo,sound & user inferface

--- ST10435609_PROG6221_POE_PART_2/MainWindow.xaml.cs
+++ ST10435609_PROG6221_POE_PART_2/MainWindow.xaml.cs
@@ -29,7 +29,60 @@ namespace ST10435609_PROG6221_POE_PART_2
 
         }// end of constructor
 
+        private void start_ai(object sender, RoutedEventArgs e)
+        {// start of method
+
+            //set the logo page grid to be invisible
+            logo_grid.Visibility = Visibility.Hidden;
+
+            //set the username page grid to be visible
+            username_grid.Visibility = Visibility.Visible;
+
+        }// end of sart_ai method
+
+
+        //i added this one, so it can be removed if not needed, but i think it will be needed to submit the name of the user
+        private void submit_name(object sender, RoutedEventArgs e)
+        {// start of submit_name method
+
+            //collect the username from the text box
+            string collect_username = user_name.Text.ToString();
+
+            //check if the name is  empty or not
+            if (collect_username !="") 
+            { // start of if statment
+
+                //show message
+                MessageBox.Show("Welcome " + collect_username + " to the awareness AI chatbot!");
+
+                //set the username page grid to be visible
+                username_grid.Visibility = Visibility.Hidden;
+
+                // set the chats page grid to be visible
+                chats_grid.Visibility = Visibility.Visible;
+
+                //add the username to the welcome message in the chats page
+                chats_list.Items.Add("Welcome " + collect_username + " to the awareness AI chatbot! How can I assist you today?");
+
+            }// end of if statemnt
+
+            else
+            {// start of else statement
+                //show message
+                MessageBox.Show("Please enter your username to continue!");
+            }//end of else statement
+
+
+
+        }// end of submit_name method
+
+        private void send_question(object sender, RoutedEventArgs e)
+        {// start of send_question method
+
+
+        }// end of send_question method
 
     }//end of class
+
 
 }// end of namespace
--- ST10435609_PROG6221_POE_PART_2/greet_user.cs
+++ ST10435609_PROG6221_POE_PART_2/greet_user.cs
@@ -1,9 +1,28 @@
-﻿namespace ST10435609_PROG6221_POE_PART_2
-{
+﻿using System;
+using System.Media;
+
+namespace ST10435609_PROG6221_POE_PART_2
+{//start of namespace
     internal class greet_user
-    {
+    {//start of class
         public greet_user()
-        {
-        }
-    }
-}
+        {// start of constructor
+
+            //auto get the path of the greeting.wav file
+            string full_path = AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug\", @"\greeting.wav");
+
+            //create an instance of the sound player class
+            //witj an object name greeting
+            SoundPlayer greeting = new SoundPlayer(full_path);
+
+            //then greet the user
+            greeting.PlaySync();
+
+        }// end of constructor
+
+
+    }// end of class
+
+}// end of namespace
+
+
