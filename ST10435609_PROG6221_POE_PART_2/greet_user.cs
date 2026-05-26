86f1aba56d407d575869c348633a6484cfb0b8f9
Sifiso Khausela <st10435609@rcconnect.edu.za>
2026-05-26 22:53:53 +0200

    updated GUI colours & styling

--- ST10435609_PROG6221_POE_PART_2/greet_user.cs
+++ ST10435609_PROG6221_POE_PART_2/greet_user.cs
@@ -2,27 +2,27 @@
 using System.Media;
 
 namespace ST10435609_PROG6221_POE_PART_2
-{//start of namespace
+{// start of namespace
+
     internal class greet_user
-    {//start of class
+    {// start of class
+
         public greet_user()
         {// start of constructor
 
-            //auto get the path of the greeting.wav file
-            string full_path = AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug\", @"\greeting.wav");
+            // auto get the path of the greeting.wav file
+            string full_path = AppDomain.CurrentDomain.BaseDirectory
+                                        .Replace(@"\bin\Debug\", @"\greeting.wav");
 
-            //create an instance of the sound player class
-            //witj an object name greeting
+            // create an instance of the sound player class
+            // with an object name greeting
             SoundPlayer greeting = new SoundPlayer(full_path);
 
-            //then greet the user
+            // then greet the user
             greeting.PlaySync();
 
         }// end of constructor
 
-
     }// end of class
 
 }// end of namespace
-
-
